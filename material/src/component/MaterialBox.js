f7511aa8b3c3920404f28819a08cfe684bdb4cf9
TahaAlTamimi <altamimitmo@gmail.com>
2019-09-01 22:42:11 +0300

    sort down bar

--- material/src/component/MaterialBox.js
+++ material/src/component/MaterialBox.js
@@ -11,9 +11,10 @@ export class MaterialBox extends Component {
             <>
             {material.map((item,index)=>{
               return(
-                <div className="container ">
+                <div className="container row justify-content-md-center">
                 <div className="card card-body mb-3">
-                <h4>{item.name}</h4>
+                <h4>{item.name} <i className="fas fa-sort-down"/>
+                </h4>
                 <ul className="list-grou ">
                    <a href="#"> <li className="list-group-item">{item.m1}</li></a>
                    <a href="#"> <li className="list-group-item">{item.m2}</li></a>
