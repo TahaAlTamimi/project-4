a06511ede8f6621fdc8b07aa992b0d6ad73d981c
TahaAlTamimi <altamimitmo@gmail.com>
2019-09-03 07:13:23 +0300

    add material to html basic

--- material/src/component/TransferToMaterial/HTMLBasic.js
+++ material/src/component/TransferToMaterial/HTMLBasic.js
@@ -1,14 +1,43 @@
 import React, { Component } from 'react'
+import Header from '../Header';
 
 export class HTMLBasic extends Component {
     state = {
+        htmlb:[]
 
+    }
+
+    componentDidMount(){
+        fetch('/material/htmlbasic')
+        .then(res=>res.json())
+        .then(htmlb=>this.setState({htmlb},()=>console.log("material",htmlb)))
+
+        .catch(err => {
+            
+            console.log("Error Reading data " + err);
+          });
     }
 
     render() {
         return (
             <div>
-            HTMLBasic done    
+                
+           Welcome
+            <ul>
+            {this.state.htmlb.map((item,index)=>{
+                return (
+                    <>
+                    <li key={index}>{item.name}</li>
+                    <li><a href={item.m1} target="_blank"> video</a></li>
+                    <li><a href={item.m2} target="_blank"> Article</a></li>
+                    <li><a href={item.m3} target="_blank"> Article</a></li>
+                    
+                    </>
+                )
+            })} 
+            </ul>
+           
+            
             </div>
         )
     }
--- server/serevr.js
+++ server/serevr.js
@@ -17,5 +17,23 @@ app.get('/material',(req,res)=>{
  
 });
 
+app.get('/material/htmlbasic',(req,res)=>{
+   const htmlb=
+  
+     [
+        {id:1,name:"HTMLBasic",m1:"http://videodictionary.kwebpia.net/?m=Video_Player_Drive&state=%7B%22ids%22:%5B%221eZnuM-1eQVMjgqOuZf8KwZg3GWLAKeq3%22%5D,%22action%22:%22open%22,%22userId%22:%22111281667878931019356%22%7D",m2:"https://www.w3schools.com/html/html_intro.asp",m3:"https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/HTML_basics"},
+
+      //   {id:2,name:"Article",m1:"https://www.w3schools.com/html/html_intro.asp",m2:"intermediate",},
+
+      //   {id:3,name:"Article",m1:"https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/HTML_basics",m2:"intermediate",},
+     ];
+   
+      
+
+             
+    res.json(htmlb);
+
+});
+
 const PORT=7777;
 app.listen(PORT,()=>console.log(`server started on port ${PORT}`))
